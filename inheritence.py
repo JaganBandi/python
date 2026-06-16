510a5d4cde31815f7b9e60ecc3032b369150de45
jagan <jaganbandi06@gmail.com>
2026-06-16 11:28:05 +0530

    aquring

---
+++ inheritence.py
@@ -0,0 +1,14 @@
+class mobile:
+
+	def switch_on(button):
+		print("Mobile Power On")
+
+class samsung(mobile):
+
+	def camera(photoes):
+		print("Mobile Is Can Capture the Pictures")
+
+s1 = samsung()
+
+s1.switch_on()
+s1.camera()
---
+++ inheritence2.py
@@ -0,0 +1,13 @@
+class laptop:
+
+	def power_on(self):
+		print("Laptop Power On")
+
+class Dell(laptop):
+
+	def coding(self):
+		print("Coding In Python")
+
+d1 = Dell()
+d1.power_on()
+d1.coding()
