0414a07a2491815646a5143942ebad93d40528df
jagan <jagan@gmail.com>
2026-08-18 11:08:41 +0530

    sys arguments

---
+++ argments.py
@@ -0,0 +1,5 @@
+import sys
+
+environment = sys.argv[1]
+
+print("Deploying to:", environment)
---
+++ args.py
@@ -0,0 +1,7 @@
+def greet(name, city):
+	print("Name:", name)
+	print("City:", city)
+
+details = ("Jagan", "Anthapur")
+
+greet(*details)
